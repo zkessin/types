b261a792731761e8b849862f1a30addcb1ee73a4
Zachary Kessin <zkessin@gmail.com>
2014-11-07 15:32:21 +0200

    create types app

---
+++ include/types.hrl
@@ -0,0 +1,6 @@
+
+-type(maybe(X)                            :: {ok, X}| not_found). 
+-type(maybe_list(X)                       :: X|[X]).
+-type(error(X)                            :: {ok, X} | {error, term()}).
+-type(maybe_error(X)                      :: {ok, X} | not_found | {error, term()}).
+-type(match(X)                            :: X|'_').
