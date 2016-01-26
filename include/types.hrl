cc8bf088b45b3647c3e980ebf2cbbd5e89d9d998
Zachary Kessin <zkessin@gmail.com>
2016-01-26 09:06:43 +0200

    Update types.hrl
    wrap in -ifndef

--- include/types.hrl
+++ include/types.hrl
@@ -1,7 +1,10 @@
 
+-ifndef(TYPES).
+-define(TYPES,true).
 -type(maybe(X)                            :: {ok, X}| not_found). 
 -type(maybe_list(X)                       :: X|[X]).
 -type(error(X)                            :: {ok, X} | {error, term()}).
 -type(maybe_error(X)                      :: {ok, X} | not_found | {error, term()}).
 -type(match(X)                            :: X|'_').
 -type(opt(X)                              :: X|undefined).
+-endif.
